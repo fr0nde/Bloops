209da516787bbed8490ebffb1643e28c186d35f1
Clément <clemvnt@gmail.com>
2019-10-03 09:42:14 +0200

    Ajout des services

---
+++ Bloops.Game/Assets/Scripts/Services/SocialService.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+public static class SocialService
+{
+    #region Static properties
+
+    public static string SocialID
+    {
+        get { return Social.localUser?.id; }
+    }
+
+    public static string SocialName
+    {
+        get { return Social.localUser?.userName; }
+    }
+
+    #endregion
+
+    #region Public static methods
+
+    public static async Task<bool> AuthenticateAsync()
+    {
+        TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+        Social.localUser.Authenticate(success => tcs.SetResult(success));
+        return await tcs.Task;
+    }
+
+    #endregion
+}
