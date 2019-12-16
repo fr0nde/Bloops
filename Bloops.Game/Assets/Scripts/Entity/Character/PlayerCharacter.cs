78b9fe9bdec94b3ad82c94a322189a4fb3d9a017
GREMY Florentin <fgremy22@gmail.com>
2019-12-16 16:44:29 +0100

    Changes the draging system

--- Bloops.Game/Assets/Scripts/Entity/Character/PlayerCharacter.cs
+++ Bloops.Game/Assets/Scripts/Entity/Character/PlayerCharacter.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class PlayerCharacter : MonoBehaviour
 {
     public float speed;             //Floating point variable to store the player's movement speed.
-    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    private Rigidbody2D character;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
     private Vector2 posFin;
     private Vector2 posDeb;
     private Camera cam;
@@ -15,38 +15,41 @@ public class PlayerCharacter : MonoBehaviour
     void Start()
     {
         //Get and store a reference to the Rigidbody2D component so that we can access it.
-        rb2d = GetComponent<Rigidbody2D>();
+        character = GetComponent<Rigidbody2D>();
         cam = Camera.main;
     }
 
     void Update()
     {
-        // Handle screen touches.
-        if (Input.GetMouseButtonDown(0))
+        if (character.velocity.magnitude == 0)
         {
-            posDeb = cam.ScreenToWorldPoint(Input.mousePosition);
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            posFin = cam.ScreenToWorldPoint(Input.mousePosition);
+            // Handle screen touches.
+            if (Input.GetMouseButtonDown(0))
+            {
+                posDeb = cam.ScreenToWorldPoint(Input.mousePosition);
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                posFin = cam.ScreenToWorldPoint(Input.mousePosition);
 
-            // Plus on rallonge le doigt sur l'axe horizontale, plus x augmente 
-            float longueurX = posFin.x - posDeb.x;
-            // Plus on rallonge le doigt sur l'axe verticale, plus y augmente 
-            float longueurY = posFin.y - posDeb.y;
+                // Plus on rallonge le doigt sur l'axe horizontale, plus x augmente 
+                float longueurX = posFin.x - posDeb.x;
+                // Plus on rallonge le doigt sur l'axe verticale, plus y augmente 
+                float longueurY = posFin.y - posDeb.y;
 
-            // Reset the force
-            rb2d.velocity = new Vector2(0f, 0f);
+                // Reset the force
+                character.velocity = new Vector2(0f, 0f);
 
-            Vector2 movement = new Vector2(longueurX, longueurY);
+                Vector2 movement = new Vector2(longueurX, longueurY);
 
-            rb2d.AddForce(new Vector2(-longueurX, -longueurY) * speed, ForceMode2D.Impulse);
+                character.AddForce(new Vector2(-longueurX, -longueurY) * speed, ForceMode2D.Impulse);
+            }
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        rb2d.velocity = Vector2.zero;
+        character.velocity = Vector2.zero;
 
         Debug.Log("Le personnage est entré dans un obstacle");
         if (collision.gameObject.tag == "Finish")
