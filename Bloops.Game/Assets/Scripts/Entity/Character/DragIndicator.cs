bc0f322c7bcaccc6b4ced1067aee9a309737304e
GREMY Florentin <fgremy22@gmail.com>
2019-12-18 15:08:45 +0100

    Completely change the DragIndicator and movement system

--- Bloops.Game/Assets/Scripts/Entity/Character/DragIndicator.cs
+++ Bloops.Game/Assets/Scripts/Entity/Character/DragIndicator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,23 +9,27 @@ public class DragIndicator : MonoBehaviour
     private Vector3 endPos;
     private Camera cam;
     private Rigidbody2D character;
-    private Vector3 characterSize;
-    public LineRenderer lr;
-    public Material mat;
-    private Vector3 posCharacterWithoutSize = Vector3.zero;
+    private float characterSize;
+    private Vector3 totalDistance;
+    private LineRenderer lr;
+    private Vector3 camOffset = new Vector3(0, 0, 10);
+    [SerializeField] private AnimationCurve ac;
 
-    Vector3 camOffset = new Vector3(0, 0, 10);
-
-    [SerializeField] AnimationCurve ac;
-    Color c1 = new Color(27, 174, 243, 1);
-    Color c2 = new Color(27, 174, 243, 0.25f);
+    public GameObject playerScript;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Get character
         character = GetComponent<Rigidbody2D>();
-        characterSize = GetComponent<CircleCollider2D>().bounds.size;
+        // Get charachter radius
+        characterSize = GetComponent<CircleCollider2D>().radius;
+        // Get camera
         cam = Camera.main;
+        // Init un go lineRenderer
+        lr = gameObject.AddComponent<LineRenderer>();
+        // Pour enlever le warning d'init de la variable...
+        if (ac == null) ac = new AnimationCurve();
     }
 
     // Update is called once per frame
@@ -36,39 +41,18 @@ public class DragIndicator : MonoBehaviour
             // Quand on appuie sur l'écran
             if (Input.GetMouseButtonDown(0))
             {
-                // Pour enlever le warning d'init de la variable...
-                if (ac == null) ac = new AnimationCurve();
-                // Récupère la position du doigt lors du premier touché de l'écran
-                startPos = cam.ScreenToWorldPoint(Input.mousePosition) + camOffset;
-                // Init un go lineRenderer si il n'y en a pas
-                if (lr == null) lr = gameObject.AddComponent<LineRenderer>();
                 // Active le lineRenderer
                 lr.enabled = true;
+                // Récupère la position du doigt lors du premier touché de l'écran
+                startPos = cam.ScreenToWorldPoint(Input.mousePosition) + camOffset;
                 // Indique au go qu'il a deux points d'ancrage
                 lr.positionCount = 2;
-                // position du personnage + sa taille
-                if (startPos.x < character.position.x)
-                {
-                    posCharacterWithoutSize.x = character.position.x - characterSize.x;
-                } else
-                {
-                    posCharacterWithoutSize.x = character.position.x + characterSize.x;
-                }
-                if (startPos.y < character.position.y)
-                {
-                    posCharacterWithoutSize.y = character.position.y - characterSize.y;
-                }
-                else
-                {
-                    posCharacterWithoutSize.y = character.position.y + characterSize.y;
-                }
                 // Set le premier point d'ancrage à la position du personnage
-                lr.SetPosition(0, posCharacterWithoutSize);
-
+                lr.SetPosition(0, character.position);
                 // Utilise les coordonées x y similaires selon l'écran
                 lr.useWorldSpace = true;
                 // set le layer du linerenderer
-                lr.sortingLayerName = "Background";
+                lr.sortingLayerName = "Player";
                 // Ajoute l'animationCurve pour un effet de triangle
                 lr.widthCurve = ac;
                 // Ajoute des bords arrondis
@@ -77,69 +61,60 @@ public class DragIndicator : MonoBehaviour
                 lr.widthMultiplier = 0.8f;
                 // Ajoute le material de base
                 lr.material = new Material(Shader.Find("Sprites/Default"));
+                // Ajoute une couleur 
                 lr.material.color = new Color32(231, 228, 227, 80);
             }
             // Quand on a toujours le doigt sur l'écran
             if (Input.GetMouseButton(0))
             {
-                // Récupère la position du doigt lors du premier touché de l'écran
+                // Récupère la position du doigt à l'instant présent
                 endPos = cam.ScreenToWorldPoint(Input.mousePosition) + camOffset;
-                // Récupère la distance entre les deux position du doigt enregistrées
-                Vector3 distance = endPos - startPos;
-                if (distance == Vector3.zero)
-                {
-                    lr.SetPosition(1, posCharacterWithoutSize);
-                } else
-                {
-                    // Récupère la pos du personnage
-                    Vector3 posPersonnage = character.position;
-                    // Ajoute la distance à la postion du personnage
-                    // Maximum de distance de 2.5f
-                    if (distance.x > 2.5f)
-                    {
-                        distance.x = 2.5f;
-                    }
-                    else if (distance.x < -2.5f)
-                    {
-                        distance.x = -2.5f;
-                    }
-                    if (distance.y > 2.5f)
-                    {
-                        distance.y = 2.5f;
-                    }
-                    else if (distance.y < -2.5f)
-                    {
-                        distance.y = -2.5f;
-                    }
 
-                    if (endPos.x < character.position.x)
-                    {
-                        posCharacterWithoutSize.x = posPersonnage.x - characterSize.x;
-                    }
-                    else
-                    {
-                        posCharacterWithoutSize.x = posPersonnage.x + characterSize.x;
-                    }
-                    if (endPos.y < character.position.y)
-                    {
-                        posCharacterWithoutSize.y = posPersonnage.y - characterSize.y;
-                    }
-                    else
-                    {
-                        posCharacterWithoutSize.y = posPersonnage.y + characterSize.y;
-                    }
+                // Récupère la position inversé
+                Vector3 reversePos = endPos - startPos;
+                // Ajoute laposition inversé a la position du personnage
+                Vector3 reversedEndPos = (Vector3)character.position + reversePos;
 
-                    Vector3 posFin = posCharacterWithoutSize + distance;
-                    // Set le deuxième points d'ancrage du linerenderer pour le dessiner
-                    lr.SetPosition(1, posFin);
-                }
+                // Calcule la position de départ
+                Vector3 posDeb = calculPos(character.position, reversedEndPos, characterSize / 2);
+                // Attribue le premier point du lineRenderer
+                lr.SetPosition(0, posDeb);
+
+                // Calcul la distance du linerenderer
+                float distTotal = Vector3.Distance(character.position, reversedEndPos);
+                // Bloque la distance a 3F
+                if (distTotal > 3F) distTotal = 3F;
+                // Calcule la position de fin 
+                Vector3 posFin = calculPos(character.position, reversedEndPos, distTotal);
+                lr.SetPosition(1, posFin);
+                // Stock la distance total du drag
+                totalDistance = posFin - posDeb;
             }
         }
         // Quand on relache le doigt de l'écran
         if (Input.GetMouseButtonUp(0))
         {
+            // Get the PlayerCharacter.cs script
+            var PlayerCharacter = playerScript.GetComponent<PlayerCharacter>();
+            PlayerCharacter.LaunchPlayer(totalDistance);
+
             //Désactive le linerenderer
             lr.enabled = false;
         }
+    }
+    Vector3 calculPos(Vector3 deb, Vector3 fin, float distance)
+    {
+        // Calcul la direction du linerenderer
+        Vector2 direction = fin - deb;
+        // Calcul l'angle du linerenderer
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        if (angle < 0f) angle += 360f;
+
+        // Calcul la position du point selon l'angle et la direction
+        // cf : https://answers.unity.com/questions/759542/get-coordinate-with-angle-and-distance.html
+        float x = Convert.ToSingle(Math.Cos(angle * Mathf.Deg2Rad));
+        float y = Convert.ToSingle(Math.Sin(angle * Mathf.Deg2Rad));
+        Vector3 newPosition = new Vector3(x, y, 0) * distance + deb;
+        return newPosition;
     }
 }
