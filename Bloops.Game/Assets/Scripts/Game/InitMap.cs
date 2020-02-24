c351c4efa73d0d4cb525889df9888e5632087e74
Alexandre Bouchard <alexandrebouchard@hotmail.fr>
2020-02-24 15:38:43 +0100

    gestion des niveaux avec du json

--- Bloops.Game/Assets/Scripts/Game/InitMap.cs
+++ Bloops.Game/Assets/Scripts/Game/InitMap.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+
 
 public class InitMap : MonoBehaviour
 {
@@ -19,6 +21,21 @@ public class InitMap : MonoBehaviour
     public BoxCollider2D prefab_finisher;
 
 
+    [System.Serializable]
+    public class Bloc
+    {
+        public string type;
+        public int pos_x;
+        public int pos_y;
+    }
+
+    [System.Serializable]
+    public class Map
+    {
+        public string name;
+        public string[] rules;
+        public Bloc[] blocs;
+    }
     public class GameObjectPosition
     {
         public int x;
@@ -38,36 +55,43 @@ public class InitMap : MonoBehaviour
      */
 
     public string levelName = "template.txt";
+    private Map map;
 
 
     void Start()
     {
-        levelName = $"niveau_{GameSceneInfo.level}.txt";
-        readFile(levelName);
+        levelName = $"niveau_{GameSceneInfo.level}.json";
+        LoadJson("niveau_1.json");
+        LoadMap();
     }
 
-    private void readFile(string file)
+    private void LoadJson(string fileName)
     {
-        IEnumerable<string> lines = System.IO.File.ReadLines($@"Assets\Resources\Map\{file}");
-        foreach (string line in lines)
+        string text = System.IO.File.ReadAllText($@"Assets\Resources\Map\{fileName}");
+
+        map =  JsonUtility.FromJson<Map>(text);
+
+    }
+
+    private void LoadMap()
+    {
+        foreach (Bloc bloc in map.blocs)
         {
-            string[] Pos = line.Split(';');
-            int xPos = System.Convert.ToInt32(Pos[0]);
-            int yPos = System.Convert.ToInt32(Pos[1]);
-            string prefab_type = Pos[2];
+            int xPos = bloc.pos_x;
+            int yPos = bloc.pos_y;
 
             gameObjectPositions.Add(new GameObjectPosition(xPos, yPos));
-            if (prefab_type == "character")
+            if (bloc.type == "character")
             {
                 GameObject character = Instantiate(prefab_character, new Vector3(xStart + (x * xPos), yStart + (y * -yPos)), Quaternion.identity);
                 character.transform.localScale += new Vector3(-0.2f, -0.2f, -0.01f);
                 InstanceReferences.Add(character);
             }
-            else if (prefab_type == "finisher")
+            else if (bloc.type == "finisher")
             {
                 Instantiate(prefab_finisher, new Vector3(xStart + (x * xPos), yStart + (y * -yPos)), Quaternion.identity);
             }
-            else if (prefab_type == "wall")
+            else if (bloc.type == "wall")
             {
                 InstanceReferences.Add(Instantiate(prefab_wall, new Vector3(xStart + (x * xPos), yStart + (y * -yPos)), Quaternion.identity));
             }
