54b243ad018c42451e1b9d4e74a88653a6ac70ec
GREMY Florentin <fgremy22@gmail.com>
2020-03-02 21:13:12 +0100

    added sounds, animations, player movement fix and endlevel data

---
+++ Bloops.Game/Assets/Scripts/Entity/Entity/MovingBloc.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBloc : MonoBehaviour
+{
+    private BoxCollider2D movingBloc;
+    private int sens = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movingBloc = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = movingBloc.transform.position.x;
+        if (sens == 0)
+        {
+            x = x + 0.1f;
+            if (x > 12)
+            {
+                sens = 1;
+            }
+        } else
+        {
+            x = x - 0.1f;
+            if (x < -12)
+            {
+                sens = 0;
+            }
+        }
+
+        movingBloc.transform.position = new Vector3(x, movingBloc.transform.position.y);
+    }
+}
---
+++ Bloops.Game/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public static AudioClip playerLaunchSound, playerHitSound1, playerHitSound2, playerHitSound3;
+    static AudioSource audioSrc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerLaunchSound = Resources.Load<AudioClip>("Sounds/launch_1");
+        playerHitSound1 = Resources.Load<AudioClip>("Sounds/impact_1");
+        playerHitSound2 = Resources.Load<AudioClip>("Sounds/impact_2");
+        playerHitSound3 = Resources.Load<AudioClip>("Sounds/impact_3");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void PlaySound (string sound)
+    {
+        audioSrc.Stop();
+
+        switch (sound)
+        {
+            case "launch_1":
+                audioSrc.PlayOneShot(playerLaunchSound);
+                break;
+            case "impact_1":
+                audioSrc.PlayOneShot(playerHitSound1);
+                break;
+            case "impact_2":
+                audioSrc.PlayOneShot(playerHitSound2);
+                break;
+            case "impact_3":
+                audioSrc.PlayOneShot(playerHitSound3);
+                break;
+        }
+    }
+}
